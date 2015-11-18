b3c8fa7384b96aa22f401a4b2551c55328cf37f9
stephaniesookim <stephaniekim@Stephanies-MBP.att.net>
2015-11-18 03:32:20 -0800

    Feature Matrix

--- Feature_Creation.R
+++ Feature_Creation.R
@@ -3,32 +3,37 @@ library(tm)
 ################## Exploring docs ##################
 ### docs is a list of 4 groups "child", "history", "religion", "science"
 ### info of the first file of the group "child"
-length(docs$child)
-inspect(docs$child[1])
+child = docs$child
+length(child)
+inspect(child[1])
 ### see content of the first file of the group "child"
-as.character(docs$child[1])
+as.character(child[1])
 
 ################# Feature Creation ###############
 
-child = docs$child
-history = docs$history
-religion = docs$religion
-science = docs$science
-
-sample = child[1]
+# Generating small sample
+index1 = sample(length(docs$child),10)
+index2 = sample(length(docs$history),10)
+sample1 = docs$child[index1]
+sample2 = docs$history[index2]
+sample = c(sample1,sample2)
 
 # Derive a dictionary of words and total number of their appearances through out the whole dataset.
 myfun = function(book){
-  count = length(book)
+  book = tm_map(book,content_transformer(tolower))
+  book = tm_map(book,removeWords,c('project','gutenberg','ebook','title','author','release','chapter'))
   dtm = DocumentTermMatrix(book,
-                           # replace non-alphabet characters (“,”, “\n”, etc) in the content with a space character
-                           # convert uppercase to lowercase characters
-                           control = list(tolower=T,removePunctuation=T))
+                           control = list(tolower=T,stopwords=T,removePunctuation=T,removeNumbers=T)
+                           )
+  dtm = removeSparseTerms(dtm,.99)
+  dtm = as.data.frame(as.matrix(dtm))
   return(dtm)
 }
 
-myfun(sample)
+result = myfun(sample)
+View(result)
 
-### remove common words i.e. Project, Gutenberg, eBook, Title, Author, Release, Chapter, etc.
+
 ### Exclude the common word features (known as stop words) listed in http://www.textfixer.com/resources/common-english-words.txt
-
+### Exclude common words
+### Tagging
