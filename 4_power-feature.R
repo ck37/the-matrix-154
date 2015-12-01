db63d4be0afb03bb6a09e55ed4cde84011fad9f8
stephaniesookim <stephaniekim@airbears2-10-142-184-51.airbears2.1918.berkeley.edu>
2015-11-30 20:49:13 -0800

    power feature function

--- 4_power-feature.R
+++ 4_power-feature.R
@@ -1,81 +1,63 @@
 library(tm)
+library(SnowballC)
 library(stringi)
 library(stringr)
 load("~/Desktop/154/data/imported-text-docs.Rdata")
 
-index1 = sample(length(docs$child),10)
-index2 = sample(length(docs$history),10)
-sample1 = docs$child[index1]
-sample2 = docs$history[index2]
-sample = c(sample1,sample2)
-sample
+sample = docs$child[1:3]
+n = length(sample)
 
-clean = function(book){
-  book = tm_map(book, content_transformer(tolower))
-  book = tm_map(book, stripWhitespace)
-  book = tm_map(book, stemDocument)
-  book = tm_map(book, removePunctuation)
-  return(book)
+power = function(doc){
+  n = length(doc)
+  power = matrix(NA,nrow=n,ncol=3,dimnames=list(seq(1,n)))
+  for(i in 1:n){
+    book = doc[i]
+    book = tm_map(book, content_transformer(tolower))
+    book = tm_map(book, stripWhitespace)
+    book = tm_map(book, stemDocument)
+    book = tm_map(book, removePunctuation)
+    text = as.character(book)
+  
+    ### 1.average word length
+    split = strsplit(text,"\\s+")[1]
+    len = sapply(split,nchar)
+    power1 = mean(len)
+  
+    ### 2.# of unique words used
+    uniq = sapply(split,unique)
+    power2 = length(uniq)
+  
+    ### 3.# of 4-digit number
+    fourdigit = str_extract(text, "\\d{4}")
+    fourdigit2 = na.omit(fourdigit)
+    power3 = length(fourdigit2)
+  
+    ######################
+    ### 5. # of numbers
+    #power5 = sum(grepl("[[:digit:]]", substring(x,seq(1,nchar(x),1),seq(1,nchar(x),1))))
+  
+    ### 9. find # of '_'
+    #power9 = length(gregexpr('_', test)[[1]])
+  
+    ### 10(1). finding number of words in each txt file
+    #power10_1 = sum(stri_count(text.file,regex="\\S+"))
+  
+    ### 10(2). finding number of sentence in each txt file
+    #power10_2 = length(gregexpr('[[:alnum:] ][.!?]', text)[[1]])
+  
+    title = names(book)
+    power[i,] = as.matrix(cbind(power1,power2,power3))
+    rownames(power)[i] = title
+  }
+  colnames(power) = c("power1","power2","power3")
+  return(power)
 }
 
-a = clean(sample)
-text = as.character(a[1])
-split = strsplit(text,"\\s+")[1]
-split ####### when you print this, you will see that there still is backslash and quotes
+power(sample)
 
-### 1.average word length
-stri_count(text,regex="\\S+") #4151 words
-len = sapply(split,nchar) #4151 words
-mean(len)
+power_feature = list()
 
-### 2. # of unique words used
-uniq = sapply(split,unique) #1681 unique words
-length(uniq)
-
-### 3. # of 4-digit number
-
-str12 <- "coihr 1234 &/()= jngm 34 ljd"
-fourdigit = str_extract(text, "\\d{4}")
-fourdigit
-fourdigit2 = na.omit(fourdigit)
-fourdigit2
-length(fourdigit2)
-
-############## Helen
-### 5.
-sum(grepl("[[:digit:]]", substring(x,seq(1,nchar(x),1),seq(1,nchar(x),1))))
-
-
-############## Dan
-#8 length of single txt file or # of words in a single txt file
-if (!exists("stringi")) {install.packages("stringi?re")}
-library(stringi)
-
-#9 find # of '_'
-
-test = "hahaa_what??__lala"
-length(gregexpr('_', test)[[1]])
-
-
-
-
-#10 
-#finding number of words in each txt file
-if (!exists("stringi")) {install.packages("stringi?re")}
-library(stringi)
-text.file <- c(
-  "this is a string current trey",
-  "nospaces",
-  "multiple    spaces",
-  "   leadingspaces",
-  "trailingspaces    ",
-  "    leading and trailing    ",
-  "just one space each")
-sum(stri_count(text.file,regex="\\S+"))
-
-#finding number of sentence in each txt file
-text <- 'Hello world!! Here are two sentences for you... how does this work?'
-length(gregexpr('[[:alnum:] ][.!?]', text)[[1]])
-
-
+for (type in names(docs)) {
+  power_feature[[type]] = power(docs[[type]])
+}
 
