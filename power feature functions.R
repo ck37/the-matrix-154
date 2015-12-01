8a3df4ec21287fd0d80c0ca6e565b329f0dd5316
dankim93 <dan_kim93@hotmail.com>
2015-11-30 22:29:24 -0800

    work in progress

--- power feature functions.R
+++ power feature functions.R
@@ -6,24 +6,20 @@ ave.word.length = function(docs){
   r = numeric()
   c = numeric()
   
-  for(i in 1:length(docs[ ,1])){     #of rows
+  for(i in 1:length(docs[ ,1])){    
     
-    for(j in 1:length(docs[1, ])){    #of col
-    
-    c[j] = nchar(colnames(docs[j]))
-    
+    for(j in 1:length(docs[1, ])){    
+        
+      c[j] = nchar(colnames(docs[j])) * docs[i,j]
+      
     }
-   
     r[i] = mean(c)
   }
-   
-return(r)
+return(r)               # returns the vector of average word length of each txt file
 }
 
 ############################
 # number of distinct words #
 ############################
-
-
 
 
