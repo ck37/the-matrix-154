f797d4e549bb58b6389429921c937e4ca992ba62
stephaniesookim <stephaniekim@Stephanies-MBP.att.net>
2015-12-04 22:06:31 -0800

    bigrams updated

--- example2.R
+++ example2.R
@@ -1,10 +1,20 @@
+library(tm)
+library(SnowballC)
+
+child = docs$child
+history = docs$history
+religion = docs$religion
+science = docs$science
+docs2 = c(child,history,religion,science)
+
+a = docs$child[1:100]
+b = docs$child[1:100]
+c = docs$religion[1:100]
+d = docs$religion[1:100]
+aa = c(a,b,c,d)
+
 #please load packages written in example.R
 load("~/Desktop/154/data/imported-text-docs.Rdata")
-
-dd = docs$child[1:100]
-bb = docs$religion[1:100]
-cc = docs$history[1:100]
-a = c(dd,bb,cc)
 
 load_stopwords = function(input_file = "inbound/common-english-words.txt", output_file = "data/stopwords.Rdata",
                           reload_file=F) {
@@ -27,17 +37,46 @@ load_stopwords = function(input_file = "inbound/common-english-words.txt", outpu
 }
 stopwords = load_stopwords()
 
+# Derive a dictionary of words/ bigrams and total number of their appearances through out the whole dataset.
+clean_documents = function(book, stopwords = c()) {
+  book = tm_map(book, content_transformer(tolower))
+  book = tm_map(book, removeWords, c('project','gutenberg','ebook','title','author','release','chapter'))
+  if (length(stopwords) > 0) {
+    book  = tm_map(book, removeWords, stopwords)
+  }
+  # NOTE: we should double-check this removePunctuation code, because it may remove the punctation
+  # without substituting spaces, which will mess up the words.
+  dtm = DocumentTermMatrix(book,
+                           control = list(tolower=T, stopwords=T, removePunctuation=T, removeNumbers=T, stemming=T)
+  )
+  dtm = removeSparseTerms(dtm, .99)
+  # Let's hold off on this part for now.
+  dtm = as.data.frame(as.matrix(dtm))
+  return(dtm)
+}
+dtm = clean_documents(dd,stopwords)
+View(dtm)
+
 
 BigramTokenizer = function(x){
   unlist(lapply(ngrams(words(x), 2), paste, collapse = " "), use.names = FALSE)
 }
 options(mc.cores=1)
 
+gutenberg = c('project','gutenberg','ebook','anyone anywhere','no cost','re-use',
+              'license','online','wwwgutenbergorg','wwwgutenbergnet','language english','ascii',
+              'httpwwwpgdpnet','first edition','second edition','title',
+              'author','release','chapter','posting','editor','translator','encoding','updated',
+              'anyone anywhere', 'at no cost and with almost', 'restrictions whatsoever','you may copy it',
+              'give it away', 'reuse it', 'under the terms', 'license included',
+              'distributed','proofread','proofreading team','character set','encoding','usascii')
+
 power_features_bigrams = function(book, stopwords = c()){
   book = tm_map(book, content_transformer(tolower))
   book = tm_map(book, removePunctuation)
+  book = tm_map(book, removeWords, gutenberg)  
   book = tm_map(book, removeNumbers)
-  book = tm_map(book, removeWords, c('project','gutenberg','ebook','title','author','release','chapter','posting','editor','translator','encoding','ascii','updated'))
+ 
   if (length(stopwords)>0){
     book  = tm_map(book, removeWords, stopwords)
   }
@@ -45,7 +84,6 @@ power_features_bigrams = function(book, stopwords = c()){
   dtm = DocumentTermMatrix(book,
                            control = list(tokenize = BigramTokenizer, stopwords=T,stemming=T))
   dtm = as.data.frame(as.matrix(dtm))
-  
   bigrams_usage = apply(dtm, MARGIN=2, FUN=function(x){ sum(!is.na(x) & x > 0) })
   filtered = bigrams_usage[which(bigrams_usage!=nrow(dtm))]
   sorted = sort(filtered,decreasing=T)[1:2950]
@@ -53,6 +91,6 @@ power_features_bigrams = function(book, stopwords = c()){
   dtm = dtm[,bigrams_freq]
   return(dtm)
 }
-
-new = power_features_bigrams(a,stopwords)
+new = power_features_bigrams(aa,stopwords)
 View(new)
+d
